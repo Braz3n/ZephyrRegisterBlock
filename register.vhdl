90b2cb031fab0b611bb4db9a9ff5f17305bedddb
zane.barker@gmail.com <zane.barker@gmail.com>
2022-02-09 18:48:53 +0900

    Named registers and added a special increment feature to the address counter.

--- register.vhdl
+++ register.vhdl
@@ -6,15 +6,16 @@ use ieee.numeric_std.all;
 
 entity dualBusRegisterBlock is
     port (
-        rst        : in std_logic;
-        clk        : in std_logic;
-        wideOutEn  : in std_logic;
-        halfOutEn  : in std_logic;
-        halfLHSel  : in std_logic;  -- Low=0, High=1
-        rw         : in std_logic;  -- Read=1, Write=0
-        addrBus    : in std_logic_vector (regAddrBusWidth-1 downto 0);
-        dataBusWide: out std_logic_vector (regDataBusWideWidth-1 downto 0);
-        dataBusHalf: inout std_logic_vector (regDataBusHalfWidth-1 downto 0)
+        rst         : in std_logic;
+        clk         : in std_logic;
+        wideOutEn   : in std_logic;
+        halfOutEn   : in std_logic;
+        halfLHSel   : in std_logic;  -- Low=0, High=1
+        rw          : in std_logic;  -- Read=1, Write=0
+        incrementPC : in std_logic;
+        addrBus     : in std_logic_vector (regAddrBusWidth-1 downto 0);
+        dataBusWide : out std_logic_vector (regDataBusWideWidth-1 downto 0);
+        dataBusHalf : inout std_logic_vector (regDataBusHalfWidth-1 downto 0)
     );
 end dualBusRegisterBlock;
 
@@ -22,7 +23,7 @@ architecture rtl of dualBusRegisterBlock is
     signal internalDataBusHalf          : std_logic_vector (regDataBusHalfWidth-1 downto 0) := (others => '0');
     signal internalDataBusWide          : std_logic_vector (regDataBusWideWidth-1 downto 0) := (others => '0');
     
-    type registerBlockType is array (0 to 3) of std_logic_vector (regDataBusWideWidth-1 downto 0);
+    type registerBlockType is array (0 to regCount-1) of std_logic_vector (regDataBusWideWidth-1 downto 0);
     signal registerBlockArray : registerBlockType := (others => (others => '0'));
 begin
     dataBusHalf <= internalDataBusHalf when halfOutEn = '1' else (others => 'Z');
@@ -33,6 +34,8 @@ begin
         if rw = '1' then
             if wideOutEn = '1' then
                 internalDataBusWide <= registerBlockArray(to_integer(unsigned(addrBus)));
+            else
+                internalDataBusWide <= (others => '0');
             end if;
 
             if halfOutEn = '1' then
@@ -41,7 +44,12 @@ begin
                 else
                     internalDataBusHalf <= registerBlockArray(to_integer(unsigned(addrBus))) (regDataBusWideWidth-1 downto regDataBusHalfWidth);
                 end if;
+            else
+                internalDataBusHalf <= (others => '0');
             end if;
+        else
+            internalDataBusWide <= (others => '0');
+            internalDataBusHalf <= (others => '0');         
         end if;
     end process;
 
@@ -50,13 +58,26 @@ begin
         if rst = '1' then
             registerBlockArray <= (others => (others => '0'));
         elsif rising_edge(clk) and rw = '0' then
-            if halfLHSel = '0' then
-                registerBlockArray(to_integer(unsigned(addrBus))) (regDataBusHalfWidth-1 downto 0) <= dataBusHalf;
+            if to_integer(unsigned(addrBus)) = PC then
+                if incrementPC = '1' then
+                    registerBlockArray(PC) <= std_logic_vector(unsigned(registerBlockArray(PC)) + 1);
+                else
+                    if halfLHSel = '0' then
+                        registerBlockArray(PC) (regDataBusHalfWidth-1 downto 0) <= dataBusHalf;
+                    else
+                        registerBlockArray(PC) (regDataBusWideWidth-1 downto regDataBusHalfWidth) <= dataBusHalf;
+                    end if;
+                end if;
             else
-                registerBlockArray(to_integer(unsigned(addrBus))) (regDataBusWideWidth-1 downto regDataBusHalfWidth) <= dataBusHalf;
+                if halfLHSel = '0' then
+                    registerBlockArray(to_integer(unsigned(addrBus))) (regDataBusHalfWidth-1 downto 0) <= dataBusHalf;
+                else
+                    registerBlockArray(to_integer(unsigned(addrBus))) (regDataBusWideWidth-1 downto regDataBusHalfWidth) <= dataBusHalf;
+                end if;
             end if;
         end if;
     end process;
+
 end rtl;
 
 
--- registerConstants.vhdl
+++ registerConstants.vhdl
@@ -7,6 +7,12 @@ package registerConstants is
     constant regAddrBusWidth     : integer := 2;
     constant regDataBusHalfWidth : integer := 8;
     constant regDataBusWideWidth : integer := regDataBusHalfWidth*2;
+    
+    -- Register names
+    constant PC : integer := 0; -- Program Counter
+    constant AB : integer := 1;
+    constant CD : integer := 2;
+    constant DE : integer := 3;
 end registerConstants;
 
 package body registerConstants is
